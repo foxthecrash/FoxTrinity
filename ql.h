f0b0ed0082f70c7bb0c36f1390c1923bff2d42e9
foxthecrash <the_vain_ever@hotmail.com>
2014-04-10 16:38:48 -0500

    DIFF

--- ql.h
+++ ql.h
@@ -80,5 +80,4 @@ struct {								\
 	qr_foreach((a_var), ql_first(a_head), a_field)
 
 #define	ql_reverse_foreach(a_var, a_head, a_field)			\
-#define	ql_reverse_foreach(a_var, a_head, a_field)			\
 	qr_reverse_foreach((a_var), ql_first(a_head), a_field)
